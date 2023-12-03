46253686f852a4e39c7443ce3d37021c831013f7
suavepek6 <151496740+suavepek6@users.noreply.github.com>
2023-12-03 16:03:16 -0500

    Add files via upload
    Uploaded project-related files

---
+++ loadAudioFile.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+gfile = ''
+# create the root window
+root = tk.Tk()
+root.title('Acoustics Sampling GUI (TESTING)')
+root.resizable(False, False)
+root.geometry('300x150')
+
+'''
+tkinter.filedialog.askopenfilenames(**options)
+Create an Open dialog and 
+return the selected filename(s) that correspond to 
+existing file(s).
+'''
+def select_file():
+    filetypes = (
+        ('WAV files', '*.wav'),
+        ('MP3 files', '*.mp3*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    gfile = filename
+
+    # tkinter.messagebox — Tkinter message prompts
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
+
+
+    gfile_label = ttk.Label(root, text=gfile)
+    gfile_label.pack(side="bottom")
+
+
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open a File',
+    command=select_file
+)
+
+open_button.pack(expand=True)
+
+
+# run the application
+root.mainloop()
